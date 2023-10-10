c60af6e6edaeafcbecf7127885f6394f2d2986f7
KKR237 <KKR237@users.noreply.github.com>
2023-10-10 13:17:50 -0400

    First

---
+++ app/src/main/java/com/keyur/assignment1/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.keyur.assignment1.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Screen"
+    }
+    val text: LiveData<String> = _text
+}
